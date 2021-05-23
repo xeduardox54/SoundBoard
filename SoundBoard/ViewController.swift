62f5c5d719d07e4798641523098bf9dd5ebc5a0c
Eduardo Rodriguez <eduardo.rodriguez@tecsup.edu.pe>
2021-05-23 10:55:23 -0700

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by Eduardo Justo Rodriguez Herrera on 5/23/21.
+//  Copyright © 2021 Eduardo Justo Rodriguez Herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
