e1ad5ee4309b3eca7c6cf09c1fae6c823c8191f5
Eduardo Rodriguez <eduardo.rodriguez@tecsup.edu.pe>
2021-05-24 22:10:38 -0700

    Adición de las funcionalidades de listado y eliminación del TableView junto con la función de guardar un audio.

---
+++ SoundBoard/SoundViewController.swift
@@ -0,0 +1,76 @@
+import UIKit
+import AVFoundation
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var grabarButton: UIButton!
+    @IBOutlet weak var reproducirButton: UIButton!
+    @IBOutlet weak var nombreTextField: UITextField!
+    @IBOutlet weak var agregarButton: UIButton!
+    
+    var grabarAudio:AVAudioRecorder?
+    var reproducirAudio:AVAudioPlayer?
+    var audioURL:URL?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configurarGrabacion()
+        reproducirButton.isEnabled = false
+        agregarButton.isEnabled = false
+    }
+    
+    func configurarGrabacion(){
+        do{
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: [])
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            let basePath:String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basePath, "audio.m4a"]
+            audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
+            
+            print("*************************")
+            print(audioURL!)
+            print("*************************")
+            
+            var settings:[String:AnyObject] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject?
+            settings[AVSampleRateKey] = 44100.0 as AnyObject?
+            settings[AVNumberOfChannelsKey] = 2 as AnyObject?
+            
+            grabarAudio = try AVAudioRecorder(url: audioURL!, settings: settings)
+            grabarAudio!.prepareToRecord()
+        }catch let error as NSError{
+            print(error)
+        }
+    }
+
+    @IBAction func grabarTapped(_ sender: Any) {
+        if grabarAudio!.isRecording{
+            grabarAudio?.stop()
+            grabarButton.setTitle("GRABAR", for: .normal)
+            reproducirButton.isEnabled = true
+        }else{
+            grabarAudio?.record()
+            grabarButton.setTitle("DETENER", for: .normal)
+            reproducirButton.isEnabled = false
+            agregarButton.isEnabled = true
+        }
+    }
+    @IBAction func reproducirTapped(_ sender: Any) {
+        do{
+            try reproducirAudio = AVAudioPlayer(contentsOf: audioURL!)
+            reproducirAudio!.play()
+            print("Reproduciendo")
+        } catch {}
+    }
+    @IBAction func agregarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let grabacion = Grabacion(context: context)
+        grabacion.nombre = nombreTextField.text
+        grabacion.audio = NSData(contentsOf: audioURL!)! as Data
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController!.popViewController(animated: true)
+    }
+}
--- SoundBoard/ViewController.swift
+++ SoundBoard/ViewController.swift
@@ -7,14 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var grabaciones:[Grabacion] = []
+    var reproducirAudio:AVAudioPlayer?
+    
+    @IBOutlet weak var tablaGrabaciones: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tablaGrabaciones.delegate = self
+        tablaGrabaciones.dataSource = self
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do{
+            grabaciones = try context.fetch(Grabacion.fetchRequest())
+            tablaGrabaciones.reloadData()
+        }catch{}
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return grabaciones.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let grabacion = grabaciones[indexPath.row]
+        cell.textLabel?.text = grabacion.nombre
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let grabacion = grabaciones[indexPath.row]
+        do{
+            reproducirAudio = try AVAudioPlayer(data:grabacion.audio! as Data)
+            reproducirAudio?.play()
+        }catch{}
+        tablaGrabaciones.deselectRow(at: indexPath, animated:true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let grabacion = grabaciones[indexPath.row]
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(grabacion)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            do{
+                grabaciones = try context.fetch(Grabacion.fetchRequest())
+                tablaGrabaciones.reloadData()
+            }catch{}
+        }
+    }
 
 }
 
